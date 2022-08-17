97b014cdd273f4d3bffbf07288414581d7dee676
hyunrrr <whgusrms96@gmail.com>
2022-08-17 14:48:03 +0900

    feat: 기본코드 추가

---
+++ infraPractice/src/main/java/com/practice/infraPractice/InfraPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.practice.infraPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraPracticeApplication.class, args);
+	}
+}
---
+++ infraPractice/src/test/java/com/practice/infraPractice/InfraPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.infraPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
